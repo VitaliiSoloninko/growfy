b44a6d17417f13288d545e4760ab9741b937d83e
22051988Di <vitalii.soloninko@gmail.com>
2024-04-26 17:20:20 +0200

    version 1.2.0	navigation by id

--- js/script.js
+++ js/script.js
@@ -1,55 +1,64 @@
-
-document.addEventListener('click', documentClick);
-
+document.addEventListener('click', documentClick)
 function documentClick(e) {
-	const targetItem = e.target;
-	console.log('targetItem');
+	const targetItem = e.target
+	console.log('targetItem')
 	if (targetItem.closest('.icon-menu')) {
-		document.documentElement.classList.toggle('menu-open');
+		document.documentElement.classList.toggle('menu-open')
 	}
 }
 
-// animation
-
-const animItems = document.querySelectorAll('._anim-items');
-
+// Animation elements
+const animItems = document.querySelectorAll('._anim-items')
 if (animItems.length > 0) {
-	window.addEventListener('scroll', animOnScroll);
-
+	window.addEventListener('scroll', animOnScroll)
 	function animOnScroll(params) {
 		for (let index = 0; index < animItems.length; index++) {
-			const animItem = animItems[index];
-			const animItemHeight = animItem.offsetHeight;
-			const animItemOffset = offset(animItem).top;
-			const animStart = 4;
+			const animItem = animItems[index]
+			const animItemHeight = animItem.offsetHeight
+			const animItemOffset = offset(animItem).top
+			const animStart = 4
 
-			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			let animItemPoint = window.innerHeight - animItemHeight / animStart
 
 			if (animItemHeight > window.innerHeight) {
-				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+				animItemPoint = window.innerHeight - window.innerHeight / animStart
 			}
 
-			if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
-				animItem.classList.add('_active');
+			if (
+				scrollY > animItemOffset - animItemPoint &&
+				scrollY < animItemOffset + animItemHeight
+			) {
+				animItem.classList.add('_active')
 			} else {
 				if (!animItem.classList.contains('_anim-no-hide')) {
-					animItem.classList.remove('_active');
+					animItem.classList.remove('_active')
 				}
 			}
 		}
 	}
 
 	function offset(el) {
-		const rect = el.getBoundingClientRect();
-		scrollLeft = window.scrollX || document.documentElement.scrollLeft,
-			scrollTop = window.scrollY || document.documentElement.scrollTop;
+		const rect = el.getBoundingClientRect()
+		;(scrollLeft = window.scrollX || document.documentElement.scrollLeft),
+			(scrollTop = window.scrollY || document.documentElement.scrollTop)
 
 		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 	}
 
 	setTimeout(() => {
-		animOnScroll();
-	}, 300);
+		animOnScroll()
+	}, 300)
+}
 
-
+// Navigation by ID - anchors
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (event) {
+		event.preventDefault() // відмінили стандарте повеение кнопки, зараз вона не працює
+		const blockID = anchor.getAttribute('href')
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	})
 }
